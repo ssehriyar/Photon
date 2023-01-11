0a8946cbbd50092bc63a2e6fa12788b1e4c4ec52
Selim ŞEHRİYAROĞLU <selimsehriyar@gmail.com>
2023-01-11 10:43:32 +0300

    Initial commit

---
+++ Assets/Scripts/PUN/NetworkController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class NetworkController : MonoBehaviourPunCallbacks
+{
+    private void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        base.OnConnectedToMaster();
+        Debug.Log("OnConnectedToMaster " + PhotonNetwork.CloudRegion);
+    }
+}
