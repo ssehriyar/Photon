bcb021d2d5bebb8e3f05056185bdfc19804592f1
Selim ŞEHRİYAROĞLU <selimsehriyar@gmail.com>
2023-01-12 15:03:13 +0300

    Dam

---
+++ Assets/Scripts/PUN/RoomButton.cs
@@ -0,0 +1,27 @@
+using Photon.Pun;
+using TMPro;
+using UnityEngine;
+
+public class RoomButton : MonoBehaviour
+{
+    private int _roomSize;
+    private int _playerCount;
+    private string _roomName;
+    
+    [SerializeField] private TMP_Text _nameText;
+    [SerializeField] private TMP_Text _sizeText;
+
+    public void JoinRoomOnClick()
+    {
+        PhotonNetwork.JoinRoom(_roomName);
+    }
+
+    public void SetRoom(string nameInput, int sizeInput, int countInput)
+    {
+        _roomName = nameInput;
+        _roomSize = sizeInput;
+        _playerCount = countInput;
+        _nameText.text = nameInput;
+        _sizeText.text = countInput + "/" + sizeInput;
+    }
+}
